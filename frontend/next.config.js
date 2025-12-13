81a64102d86eb10287adc1aec754c94681a8e40f
camgraphe <198688581+camgraphe@users.noreply.github.com>
2025-12-13 23:27:50 +0100

    Add redirect for blog.maxvideoai.com storyboard link

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -55,6 +55,12 @@ const nextConfig = {
         permanent: true,
       },
       {
+        source: '/storyboard-sora-pro',
+        has: [{ type: 'host', value: 'blog.maxvideoai.com' }],
+        destination: 'https://maxvideoai.com/blog/sora-2-sequenced-prompts',
+        statusCode: 301,
+      },
+      {
         source: '/fr/pricing-calculator',
         destination: '/fr/tarifs',
         statusCode: 301,
